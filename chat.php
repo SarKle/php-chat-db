85cd712fa0d53054905ce58ce17ab71430fb105e
sarahklewiec <sarahklewiec@gmail.com>
2018-03-12 10:12:30 +0100

    lastversion

--- chat.php
+++ chat.php
@@ -24,7 +24,7 @@ include"conversation.php";
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <lin rel="stylesheet" type="text/css" href="chat.css">
+  <link rel="stylesheet" type="text/css" href="chat.css">
   <title>MYCHAT</title>
 </head>
 <body>
